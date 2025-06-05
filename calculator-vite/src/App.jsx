5e212d6142a4b6eefa934b95ca4292e9d300c9ac
Kurt Justine Waga <kurtwaga46@gmail.com>
2025-06-05 16:53:48 +0800

    Initial Commit

---
+++ calculator-vite/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calculator from './Calculator.jsx';
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', padding: '40px' }}>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
